366fe9920c005ba39e6341b51861af4ed315f38d
Debbie Davis <disneydeb59@gmail.com>
2018-01-23 19:01:32 -0800

    day 2 stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,7 @@
-var sentence ="I am learning to code";
-console.log(sentence);
-
-var firstNumber =22;//declare and define the variable
-  console.log(firstNumber);
-
-var secondNumber = 11;
-  console.log(secondNumber)
-{
-const truth ='Ep8 was...TERRIBLE...';
-  console.log(truth);
-}
-var truth ='Ep8 was...TERRIBLE...'; //what is the difference b/t redeclare and redefine a var
-    console.log(truth);
-let rightNow = new Date(); //anything in a parenthesis () is a function
-  console.log(rightNow)
-
-var iLikePi = Math.PI
-  console.log(iLikePi);
-
-var string = "lalalalal!";
-
-var newString = newString("lalalala");
-
-  console.log(string);
-  console.log(newString.toUpperCase());
+var firstName = prompt("what is your first name?");
+console.log(firstName);
+var lastName = prompt("what is your last name?");
+console.log(lastName);
+alert(lastName);
+let birthday = prompt("What's your birthday?, please answer in the following format 01/01/2018");
+confirm(new Date(birthday));
